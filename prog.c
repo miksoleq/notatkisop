67538a7b664d0ac57f8a008518a0e1cc88c43a73
miksoleq <mikolajzawrotniak@gmail.com>
2025-10-28 01:24:48 +0100

    Add files via upload

---
+++ prog.c
@@ -0,0 +1,82 @@
+#include <unistd.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
+
+#define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
+
+#define MAX_BUFFER 256
+
+void scan_dir(const char* path,const char * ext)
+{
+    DIR* dirp;
+    struct dirent *dp;
+    struct stat filestat;
+    if((dirp=opendir("."))==NULL)
+        ERR("opendir");
+    fprintf(stdout,"path: %s\n", path);
+    do {
+    errno=0;
+    if((dp=readdir(dirp))!=NULL)
+    {
+        if(lstat(dp->d_name, &filestat))
+            ERR("lstat");
+        if(ext !=NULL)
+        {
+            char*dot=strrchr(dp->d_name, '.');
+            if(!dot || strcmp(dot+1,ext)!=0)
+                continue;
+        }
+        fprintf(stdout, "%s %lu\n",dp->d_name,filestat.st_size);
+    }
+    }while (dp!=NULL);
+
+    if(errno!=0)
+        ERR("readdir");
+    if(closedir(dirp))
+        ERR("closedir");
+}
+
+
+int main(int argc, char** argv)
+{
+    int c;
+    char *path;
+    char **paths = NULL;
+    int npaths=0;
+    char *extention=NULL;
+    while((c=getopt(argc, argv, "p:e:"))!= -1)
+    {
+        switch (c) 
+        {
+            case 'e': 
+            extention=optarg;
+            break;
+            case 'p':
+            paths=realloc(paths, (npaths+1)*sizeof(char*));
+            if(!paths) ERR("realloc");
+            paths[npaths]=optarg;
+            npaths++;
+            break;
+            default:
+            break;
+        }
+    }
+    for(int i=0;i<npaths;i++)
+    {
+
+        if((path=getcwd(NULL, 0))==NULL)
+                ERR("getcwd");
+            if(chdir(paths[i]))
+                ERR("chdir");
+            scan_dir(paths[i],extention);
+            if(chdir(path))
+                ERR("chdir");
+        free(path);    
+    }
+    free(paths);
+    
+}
